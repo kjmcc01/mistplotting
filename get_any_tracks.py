1ea09bec75303bd9ea8153b15278aea9be1767f8
kjmcc01 <kjmcconnell11@gmail.com>
2024-04-10 19:58:27 -0400

    tracks fix for real this time?

--- get_any_tracks.py
+++ get_any_tracks.py
@@ -8,7 +8,7 @@ def get_mistery_tracks(masses, metallicity):
     if len([masses]) > 1:
         tracks = mistery.get_tracks(Ms=masses, FeH=metallicity)
 
-    elif len(masses) == 1:
+    elif len([masses]) == 1:
         tracks = mistery.get_track(M=masses, FeH=metallicity)
 
 
@@ -17,10 +17,10 @@ def get_mistery_tracks(masses, metallicity):
 
 def get_mistery_isochrones(times, metallicity):
 
-    if len(times) > 1:
+    if len([times]) > 1:
         isochrones = mistery.get_isochrones(ts=times, FeH=metallicity)
 
-    elif len(times) == 1:
+    elif len([times]) == 1:
         isochrones = mistery.get_isochrone(t=times, FeH=metallicity)
 
 
