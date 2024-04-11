7ee7ab821e35a79ba0e6009626e0621452cc2484
kjmcc01 <kjmcconnell11@gmail.com>
2024-04-10 20:01:13 -0400

    separate graphs

---
+++ plot_default.py
@@ -0,0 +1,22 @@
+
+# plots default tracks and isochrones for given inputs
+
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import mistery
+from get_any_tracks import get_mistery_isochrones, get_mistery_tracks
+
+def plot_default_tracks(masses, times, metallicity):
+
+    track = get_mistery_tracks(masses, metallicity)
+    isochrone = get_mistery_isochrones(times, metallicity)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.semilogy(10**track['log_Teff'], 10**track['log_L'], label='track')
+    ax2.semilogy(10**isochrone['log_Teff'], 10**isochrone['log_L'], label='isochrone')
+    plt.xlabel('effective temperature (K)')
+    plt.ylabel('luminosity (solar units)')
+    plt.xlim(8500, 2500)
+    plt.legend()
+    st.pyplot(fig)
