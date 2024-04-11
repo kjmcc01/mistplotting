cc930bd18d452e808007649cc6588c2172e39ae0
kjmcc01 <kjmcconnell11@gmail.com>
2024-04-10 20:10:07 -0400

    separate graphs

--- plot_default.py
+++ plot_default.py
@@ -12,11 +12,16 @@ def plot_default_tracks(masses, times, metallicity):
     track = get_mistery_tracks(masses, metallicity)
     isochrone = get_mistery_isochrones(times, metallicity)
 
-    fig, (ax1, ax2) = plt.subplots(2)
+    fig, (ax1, ax2) = plt.subplots(2, figsize=(5,8))
+
     ax1.semilogy(10**track['log_Teff'], 10**track['log_L'], label='track')
     ax2.semilogy(10**isochrone['log_Teff'], 10**isochrone['log_L'], label='isochrone')
-    plt.xlabel('effective temperature (K)')
-    plt.ylabel('luminosity (solar units)')
+
+    ax1.set_xlabel('effective temperature (K)')
+    ax1.set_ylabel('luminosity (solar units)')
+    ax2.set_xlabel('effective temperature (K)')
+    ax2.set_ylabel('luminosity (solar units)')
+    
     plt.xlim(8500, 2500)
     plt.legend()
     st.pyplot(fig)
